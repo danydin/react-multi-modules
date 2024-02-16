3639af15fc5ee57f3c30979d8bd4ad0c537015ea
dean ben <19169228+danydin@users.noreply.github.com>
2024-02-16 17:29:34 +0800

    completed load more data gallery async

--- react-apps/src/components/load-more-gallery/index.jsx
+++ react-apps/src/components/load-more-gallery/index.jsx
@@ -6,15 +6,17 @@ export default function LoadMoreGallery() {
     const [loading, setLoading] = useState(false)
     const [products, setProducts] = useState([])
     const [loadMoreButton, setLoadMoreButton] = useState(0)
+    const [disableButton, setDisableButton] = useState(false)
 
     async function fetchProducts() {
         try {
             setLoading(true)
             const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${loadMoreButton * 20}`)
-            const result = await response.json()
-            if (result && result.products && result.products.length) {
-                setProducts(result.products)
+            const results = await response.json()
+            if (results && results.products && results.products.length) {
+                setProducts((prevData) => [...prevData, ...results.products])
                 setLoading(false)
+                console.log(results.products)
             }
         } catch (err) {
             console.error(err)
@@ -24,11 +26,15 @@ export default function LoadMoreGallery() {
 
     useEffect(() => {
         fetchProducts()
-    },[])
+    }, [loadMoreButton])
+
+    useEffect(() => {
+        products && products.length === 100 ? setDisableButton(true) : setDisableButton(false)
+    }, [products])
 
     if (loading) return <div>Loading data!</div>
 
-    return <div className="container load-more-gallery">
+    return <div className="load-more-gallery-container">
         <div className='products'>
             {products && products.length ?
                 products.map(item =>
@@ -40,7 +46,8 @@ export default function LoadMoreGallery() {
                 : null}
         </div>
         <div className='button-container'>
-            <button>Load More</button>
+            <button disabled={disableButton} onClick={() => setLoadMoreButton(loadMoreButton + 1)}>Load More</button>
+            {disableButton ? <p>You have loaded 100 products.</p> : null}
         </div>
     </div>
 }
