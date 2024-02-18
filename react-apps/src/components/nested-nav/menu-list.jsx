db2632ee9edc6fdae1e2a00444b095a8d1a591cc
dean ben <19169228+danydin@users.noreply.github.com>
2024-02-18 14:16:37 +0800

    completed recursive nested navigation

--- react-apps/src/components/nested-nav/menu-list.jsx
+++ react-apps/src/components/nested-nav/menu-list.jsx
@@ -2,7 +2,7 @@ import MenuItem from "./menu-item";
 import './style.css'
 
 export default function MenuList({list=[]}){
-    return <ul className="menu-list-container">
+    return <ul className="menu-list-ul">
         {list && list.length? list.map(listItem => <MenuItem item={listItem}></MenuItem>) : 'no menu objects'}
     </ul>
 }
